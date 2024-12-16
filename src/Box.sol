aeec746d784f47c01e47a666bd14455c946da182
zntb <zenetibi@yahoo.com>
2024-12-16 11:38:31 +0200

    Testing the governance smart contract

--- src/Box.sol
+++ src/Box.sol
@@ -1,20 +1,23 @@
+// contracts/Box.sol
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.0;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Box is Ownable {
-    uint256 private s_number;
+    uint256 private value;
 
-    event NumberChanged(uint256 number);
+    // Emitted when the stored value changes
+    event ValueChanged(uint256 newValue);
 
-    function store(uint256 newNumber) public onlyOwner {
-        s_number = newNumber;
-        emit NumberChanged(newNumber);
+    // Stores a new value in the contract
+    function store(uint256 newValue) public onlyOwner {
+        value = newValue;
+        emit ValueChanged(newValue);
     }
 
-    function getNumber() external view returns (uint256) {
-        return s_number;
+    // Reads the last stored value
+    function retrieve() public view returns (uint256) {
+        return value;
     }
 }
